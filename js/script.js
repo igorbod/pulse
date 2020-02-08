243f22a70952c76fdb3d29ea4a42c493e78e8629
Igor Bodyagin <igorbod.webdev@gmail.com>
2020-02-08 10:09:11 +0700

    change slider from slick to tiny-slider without JQuery

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
-$(document).ready(function () {
-  $('.carousel__inner').slick({
-    infinite: true,
-    speed: 1200,
-    prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/left-arrow.png"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/right-arrow.png"></button>',
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          dots: true,
-          arrows: false
-        }
-      }
-    ]
-  });
+// $(document).ready(function () {
+//   $('.carousel__inner').slick({
+//     infinite: true,
+//     speed: 1200,
+//     prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/left-arrow.png"></button>',
+//     nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/right-arrow.png"></button>',
+//     responsive: [
+//       {
+//         breakpoint: 992,
+//         settings: {
+//           dots: true,
+//           arrows: false
+//         }
+//       }
+//     ]
+//   });
+// });
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
 });
